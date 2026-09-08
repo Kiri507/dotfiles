78fd9a41ef134fa9d9d48e19a3eb91fb4b1b4970
Kiri507 <ksverkunov@icloud.com>
2026-09-08 18:38:25 +0200

    Update config.h
    added new color and keybinds

--- dwm/config.h
+++ dwm/config.h
@@ -6,13 +6,13 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "terminus:size=14" };
+static const char dmenufont[]       = "terminus:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#09599A";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -64,13 +64,13 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        			function        argument */
-	{ MODKEY,                       XK_p,      			spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_d,      			spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, 			spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      			togglebar,      {0} },
 	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      			incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      			incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_p,      			incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      			setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      			setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,      			zoom,           {0} },
@@ -119,4 +119,5 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
+
 
